fa332f252e825da0b4de29d9ad5e969afea9d099
gijsschlief <160142703+gijsschlief@users.noreply.github.com>
2025-07-08 17:07:43 +0200

    fixed the triangular profile generation in the force profile generator and updated the data generator to use the new method

--- forward_model/data_visualizer.py
+++ forward_model/data_visualizer.py
@@ -94,7 +94,7 @@ def visualize_force_comparison(
 
 def main() -> None:
     """Load and visualize forward model data."""
-    data = forward_data_reader(file_path="/home/gijs/Desktop/Thesis/data/raw/training_combined_1000_samples_0702_1451.json")
+    data = forward_data_reader(file_path="/home/gijs/Desktop/Thesis/data/raw/training_triangular_profiles_1000_samples_0708_1427.json")
     if data is None:
         logging.error("Failed to load the forward model data.")
         return
@@ -106,7 +106,7 @@ def main() -> None:
         return
 
     old_data = forward_data_reader(
-        file_path="/home/gijs/Desktop/Thesis/data/raw/training_triangular_profiles_1000_samples_0708_1427.json"
+        file_path="/home/gijs/Desktop/Thesis/data/raw/training_triangular_profiles_1000_samples_0708_1457.json"
     )
     if old_data is None:
         logging.error("Failed to load the forward model data.")
