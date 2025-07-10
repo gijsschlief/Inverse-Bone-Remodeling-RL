81cab17b93cacf219c43ac5ffbf821f4b5df3b6d
gijsschlief <160142703+gijsschlief@users.noreply.github.com>
2025-07-10 17:48:26 +0200

    Still working on loading the latest agent file.

--- rl_model_improved_actionspace/environment.py
+++ rl_model_improved_actionspace/environment.py
@@ -1,6 +1,7 @@
 """Training environment for reinforcement learning in bone remodeling simulation."""
 
 import logging
+import os
 from pathlib import Path
 from typing import cast
 
@@ -26,7 +27,6 @@ from gymnasium import Env, spaces
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.vec_env import DummyVecEnv
-from torch import Tensor
 from torch.nn import Module
 
 
@@ -37,7 +37,7 @@ class BoneRemodellingEnvironment(Env):
 
     def __init__(
         self,
-        model_path: str,
+        surrogate_model_path: Path,
         target_densities: np.ndarray,
         target_forces: np.ndarray,
         max_steps: int = 50,
@@ -52,18 +52,18 @@ class BoneRemodellingEnvironment(Env):
 
         # Load the surrogate model and normalization parameters
         surrogate_model_and_normalization_params = load_surrogate_model(
-            model_path, ReversedSurrogateModel
+            surrogate_model_path, ReversedSurrogateModel
         )
         assert (
             surrogate_model_and_normalization_params is not None
-        ), f"Failed to load surrogate model from {model_path}"
+        ), f"Failed to load surrogate model from {surrogate_model_path}"
 
         self.surrogate_model, self.x_mean, self.x_std, self.y_mean, self.y_std = (
             surrogate_model_and_normalization_params
         )
         if self.surrogate_model is None:
             raise ValueError(
-                f"Surrogate model could not be loaded from {model_path}. Please check the file path and model type."
+                f"Surrogate model could not be loaded from {surrogate_model_path}. Please check the file path and model type."
             )
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.surrogate_model.to(self.device).eval()
@@ -367,9 +367,8 @@ class BoneRemodellingEnvironment(Env):
         # unnormalize
         if self.y_mean is not None and self.y_std is not None:
             density_tensor = unnormalize_data(density_tensor, self.y_mean, self.y_std)
-        if density_tensor is Tensor:
-            surrogate_density: np.ndarray = density_tensor.detach().cpu().numpy()
 
+        surrogate_density: np.ndarray = density_tensor.detach().cpu().numpy()
         return surrogate_density.reshape(self.return_shape)
 
 
@@ -430,39 +429,74 @@ class RewardSavingCallback(BaseCallback):
         if self.verbose:
             logging.info(f"Saved reward plot to {self.out_path}")
 
+def save_model_safely(model: PPO, path: Path) -> Path:
+    """Save the model to a file, ensuring no overwriting of existing files."""
+    if os.path.exists(path):
+        base_path, ext = os.path.splitext(path)
+        counter = 1
+        while os.path.exists(f"{base_path}_{counter}{ext}"):
+            counter += 1
+        path = Path(f"{base_path}_{counter}{ext}")
+    model.save(path)
+    return Path(path)
 
-def main() -> None:
-    """Demonstrates the environment and reward calculation."""
-    directory_path = Path(
-        "/home/gijs/Desktop/Thesis/data/raw/training_triangular_profiles_10000_samples_0708_1959.json"
-    )
-    result = forward_data_reader(directory_path)
+def find_latest_agent(path: Path) -> Path | None:
+    """Find the latest agent file in the specified directory."""
+    if os.path.exists(path):
+        base_path, ext = os.path.splitext(path)
+        counter = 1
+    while os.path.exists(f"{base_path}_{counter}{ext}"):
+        counter += 1
+    return Path(f"{base_path}_{counter}{ext}")
+
+def main(agent_path: Path, data_path: Path, surrogate_path: Path) -> None:
+    """Designs and trains a reinforcement learning agent for bone remodeling.
+
+    This function initializes the bone remodeling environment, loads the surrogate model,
+    and trains a reinforcement learning agent using the Proximal Policy Optimization (PPO)
+    algorithm. It reads training data, splits it into training sets, and uses callbacks for
+    rendering and saving rewards during training.
+
+    Args:
+    ----
+        agent_path (Path): Path to the agent model file.
+        data_path (Path): Path to the data file for training.
+        surrogate_path (Path): Path to the surrogate model file.
+
+    """
+    result = forward_data_reader(data_path)
     assert result is not None, "Failed to read data from the specified path."
     _, target_forces, target_densities = result
 
     train_densities, _, _, train_forces, _, _ = splitting(
         target_densities, target_forces, random_state=0
     )
+    logging.info(f"Training RL agent on {len(train_densities)} samples.")
 
     remodeling_environment = BoneRemodellingEnvironment(
-        model_path="/home/gijs/Desktop/Thesis/data/models/trained_model_1.pth",
+        surrogate_model_path=surrogate_path,
         target_densities=train_densities,
         target_forces=train_forces,
-        max_steps=10,
+        max_steps=100,
     )
 
-    if Path("data/agents/trained_agent_special.zip").exists():
-        model = PPO.load(
-            "data/agents/trained_agent_special.zip", env=remodeling_environment
-        )
-        model.set_env(remodeling_environment)
-    else:
+    logging.info("Environment created,loading agent if it exists.")
+    if agent_path is None:
         model = PPO("MlpPolicy", remodeling_environment, verbose=1)
+    else:
+        agent_path = find_latest_agent(agent_path)
+        if agent_path is None:
+            model = PPO("MlpPolicy", remodeling_environment, verbose=1)
+        else:
+            model = PPO.load(agent_path, env=remodeling_environment)
+            model.set_env(remodeling_environment)
+
+    logging.info(f"Starting training with agent at {agent_path if agent_path else 'new model'}.")
 
     model.learn(
-        total_timesteps=100,
+        total_timesteps=1000,
         callback=[
-            RenderCallback(render_freq=1),
+            RenderCallback(render_freq=100),
             RewardSavingCallback(
                 out_path="/home/gijs/Desktop/Thesis/data/figures/reward_curve_RL_special.png"
             ),
@@ -470,8 +504,11 @@ def main() -> None:
     )
     logging.info("Training complete.")
 
-    model.save("/home/gijs/Desktop/Thesis/data/agents/trained_agent_special_1.zip")
-
+    saved_path = save_model_safely(model, agent_path)
+    logging.info(f"Model saved to {saved_path}")
 
 if __name__ == "__main__":
-    main()
+    AGENT_PATH = Path("/home/gijs/Desktop/Thesis/data/agents/trained_agent_special.zip")
+    DATA_PATH = Path("/home/gijs/Desktop/Thesis/data/raw/training_triangular_profiles_10000_samples_0708_1959.json")
+    SURROGATE_PATH = Path("/home/gijs/Desktop/Thesis/data/models/trained_model_1.pth")
+    main(agent_path=AGENT_PATH, data_path=DATA_PATH, surrogate_path=SURROGATE_PATH)
