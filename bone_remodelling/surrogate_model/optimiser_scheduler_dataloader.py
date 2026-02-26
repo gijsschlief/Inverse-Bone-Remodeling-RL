a122c59e4460f10e6914e2e48bc352a88cefac1e
gijsschlief <160142703+gijsschlief@users.noreply.github.com>
2026-02-26 10:46:27 +0100

    added the weight decay to the optimiser where it should be

--- bone_remodelling/surrogate_model/optimiser_scheduler_dataloader.py
+++ bone_remodelling/surrogate_model/optimiser_scheduler_dataloader.py
@@ -12,7 +12,7 @@ def build_optimizer(
     train_parameters: TrainParameters,
 ) -> torch.optim.AdamW:
     """Get an optimizer for the surrogate model."""
-    return torch.optim.AdamW(model.parameters(), lr=train_parameters.learning_rate)
+    return torch.optim.AdamW(model.parameters(), lr=train_parameters.learning_rate, weight_decay=train_parameters.weight_decay)
 
 
 def build_learning_rate_scheduler(
