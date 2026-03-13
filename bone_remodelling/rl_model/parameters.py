f3bed54bba580c96d0f1061439e0564ea06dc428
gijsschlief <160142703+gijsschlief@users.noreply.github.com>
2026-03-13 01:26:32 +0100

    update parameters for curriculum learning and validation frequency

--- bone_remodelling/rl_model/parameters.py
+++ bone_remodelling/rl_model/parameters.py
@@ -29,7 +29,7 @@ class RLParameters:
 
     """
 
-    max_steps: int = 50
+    max_steps: int = 5
     force_boundary: float = 200
     density_constraint: float = 1.73
     n_steps: int = 2048
@@ -63,10 +63,10 @@ class RunConfiguration:
     output_dir: Path
     forward_type: str = "surrogate"
     total_timesteps: int = 10_000_001
-    render_frequency: int = 102_400
+    render_frequency: int =  1024 # 102_400
     validation_frequency: int = 10_000 # 200_000
     validation_size: int = 100
-    number_of_environments: int = 10
+    number_of_environments: int = 1 # 10
     random_state: int = 0
     reward_plot_path: Path = field(init=False)
     agent_path: Path = field(init=False)
